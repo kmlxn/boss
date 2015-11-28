c7e25c39651942b59182832b8043ad17576dba59
John Doe <JohnDoe@email.com>
2015-11-28 16:22:58 +0500

    Some layout changes, fixes. Some cleanup in code. Using local copy of nicEdit wysiwyg editor.

--- forum/forms.py
+++ forum/forms.py
@@ -13,22 +13,22 @@ class CommentForm(forms.Form):
 class AddTopicForm(forms.Form):
     topic_name = forms.CharField(label="Topic's name", max_length=255,
         widget=forms.TextInput(
-            attrs={"placeholder": "Name", "id": "topic_name", "class": "form-control"}
+            attrs={"placeholder": "Name", "id": "topic_name"}
         )
     )
     topic_text = forms.CharField(label="Topic's text",
         widget=forms.Textarea(
-            attrs={"rows": "3", "id": "topic_text", "class": "form-control"}
+            attrs={"rows": "3", "id": "topic_text"}
         )
     )
     topic_description = forms.CharField(label="Topic's description",
         widget=forms.Textarea(
-            attrs={"id": "topic_description", "class": "form-control"}
+            attrs={"id": "topic_description"}
         )
     )
     topic_category = forms.CharField(label="Topic's category", max_length=255,
         widget=forms.TextInput(
-            attrs={"id": "topic_category", "class": "form-control"}
+            attrs={"id": "topic_category"}
         )
     )
     topic_image = forms.FileField(required=False)
